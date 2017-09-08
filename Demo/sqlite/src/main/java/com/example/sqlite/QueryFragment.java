3bc52e644c74877a907aa1c59af8d835fdb253e2
HeXue <1016852579@qq.com>
2017-09-08 17:37:47 +0800

    Revert "增删改查"
    This reverts commit 2d5f741ae1887f687ae288c5e512131de6f6e32a.

--- Demo/sqlite/src/main/java/com/example/sqlite/QueryFragment.java
+++ Demo/sqlite/src/main/java/com/example/sqlite/QueryFragment.java
@@ -1,15 +1,12 @@
 package com.example.sqlite;
 
 import android.app.Fragment;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -20,32 +17,13 @@ public class QueryFragment extends android.support.v4.app.Fragment {
     public QueryFragment(){
 
     }
-    private Button btn_query;
-    private SQLiteDatabase db;
-    private MyDBOpenHelper myDBHelper;
-    private StringBuilder sb;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_query,container,false);
 
-        btn_query = (Button) view.findViewById(R.id.query);
-        btn_query.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sb = new StringBuilder();
-                //参数依次是:表名，列名，where约束条件，where中占位符提供具体的值，指定group by的列，进一步约束
-                //指定查询结果的排序方式
-                Cursor cursor = db.query("person", null, null, null, null, null, null);
-                if (cursor.moveToFirst()) {
-                    do {
-                        int pid = cursor.getInt(cursor.getColumnIndex("personid"));
-                        String name = cursor.getString(cursor.getColumnIndex("name"));
-                        sb.append("id：" + pid + "：" + name + "\n");
-                    } while (cursor.moveToNext());
-                }
-            }
-        });
+
         return view;
     }
 }
--- Demo/sqlite/src/main/java/com/example/sqlite/UpdateFragment.java
+++ Demo/sqlite/src/main/java/com/example/sqlite/UpdateFragment.java
@@ -1,8 +1,6 @@
 package com.example.sqlite;
 
 import android.app.Fragment;
-import android.content.ContentValues;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.text.LoginFilter;
@@ -10,7 +8,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -22,22 +19,11 @@ public class UpdateFragment extends android.support.v4.app.Fragment {
     public UpdateFragment (){
 
     }
-    private Button btn_update;
-    private SQLiteDatabase db;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_update,container,false);
-        btn_update = (Button) view.findViewById(R.id.btn_query);
-        btn_update.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ContentValues values2 = new ContentValues();
-                values2.put("name", "嘻嘻~");
-                //参数依次是表名，修改后的值，where条件，以及约束，如果不指定三四两个参数，会更改所有行
-                db.update("person", values2, "name = ?", new String[]{"呵呵~2"});
-            }
-        });
         return view;
     }
 }
